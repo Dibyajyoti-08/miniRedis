9cf9db3043114626fde03f3096280e82b6e2d4af
djena <114151571+Dibyajyoti-08@users.noreply.github.com>
2024-08-23 21:59:41 +0530

    Update ProtocolHandler class for wired protocol

--- main.py
+++ main.py
@@ -13,11 +13,82 @@ class Disconnect(Exception): pass
 Error = namedtuple('Error', ('message',))
 
 class ProtocolHandler(object):
+    def __init__(self):
+        self.handlers = {
+            '+': self.handle_simple_string,
+            '-': self.handle_error,
+            ':': self.handle_integer,
+            '$': self.handle_string,
+            '*': self.handle_array,
+            '%': self.handle_dict
+        }
     def handle_request(self, socket_file):
-        pass
+        first_byte = socket_file.read(1)
+        if not first_byte:
+            raise Disconnect()
+
+        try:
+            # Delegate to the appropriate handler based on the first byte
+            return self.handlers[first_byte](socket_file)
+        except KeyError:
+            raise CommandError('bad request')
+
+    def handle_simple_string(self, socket_file):
+        return socket_file.readline().rstrip('\r\n')
+
+    def handle_error(self, socket_file):
+        return Error(socket_file.readline().rstrip('\r\n'))
+
+    def handle_integer(self, socket_file):
+        return int(socket_file.readline().rstrip('\r\n'))
+
+    def handle_string(self, socket_file):
+        length = int(socket_file.readline().rstrip('\r\n'))
+        if length == -1:
+            return None  # Special-case for NULLs.
+        length += 2 # Include the trailing \r\n in count.
+        return socket_file.read(length)[:-2]
+
+    def handle_array(self, socket_file):
+        num_elements = int(socket_file.readline().rstrip('\r\n'))
+        return [self.handle_request(socket_file) for _ in range(num_elements)]
+
+    def handle_dict(self, socket_file):
+        num_items = int(socket_file.readline().rstrip('\r\n'))
+        elements = [self.handle_request(socket_file) for _ in range(num_items * 2)]
+        return dict(zip(elements[::2], elements[1::2]))
 
     def write_response(self, socket_file, data):
-        pass
+        def write_response(self, socket_file, data):
+            buf = BytesIO()
+            self._write(buf, data)
+            buf.seek(0)
+            socket_file.write(buf.getvalue())
+            socket_file.flush()
+
+        def _write(self, buf, data):
+            if isinstance(data, str):
+                data = data.encode('utf-8')
+
+            if isinstance(data, bytes):
+                buf.write('$%s\r\n%s\r\n' % (len(data), data))
+            elif isinstance(data, int):
+                buf.write(':%s\r\n' % data)
+            elif isinstance(data, Error):
+                buf.write('-%s\r\n' % Error.message)
+            elif isinstance(data, (list, tuple)):
+                buf.write('*%s\r\n' % len(data))
+                for item in data:
+                    self._write(buf, item)
+            elif isinstance(data, dict):
+                buf.write('%%%s\r\n' % len(data))
+                for key in data:
+                    self._write(buf, key)
+                    self._write(buf, data[key])
+            elif data is None:
+                buf.write('$-1\r\n')
+            else:
+                raise CommandError('unrecognized type: %s' % type(data))
 
 class Server(object):
     def __init__(self, 127.0.0.1, port=8096, max_client=64):
@@ -52,19 +123,3 @@ class Server(object):
 
         def run(self):
             self._server.server_forever()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
